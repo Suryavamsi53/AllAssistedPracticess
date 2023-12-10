d7432433a9cde3b62dd163421b3e57309c365bec
suryavamsi53 <63606535+Suryavamsi53@users.noreply.github.com>
2023-12-10 15:18:00 +0530

    Add project files.

---
+++ Phase4Section1.4/GoogleSearch.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+
+
+namespace Phase4Section1._4
+{
+        [TestClass]
+        public class GoogleSearch
+        {
+            [TestMethod]
+            public void FFSearch()
+            {
+                using (var driver = new FirefoxDriver())
+                {
+
+                    driver.Manage().Window.Maximize();
+                    driver.Navigate().GoToUrl("https://www.google.com");
+
+             
+
+            }
+            }
+        }
+    }
